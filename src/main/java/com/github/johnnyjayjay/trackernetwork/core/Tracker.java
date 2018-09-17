2214bc17778fd97f31f375f137b20f19e04c621d
Unknown <johnnyjayjay02@gmail.com>
2018-09-17 20:55:20 +0200

    deleted target

--- src/main/java/com/github/johnnyjayjay/trackernetwork/core/Tracker.java
+++ src/main/java/com/github/johnnyjayjay/trackernetwork/core/Tracker.java
@@ -1,8 +1,10 @@
 package com.github.johnnyjayjay.trackernetwork.core;
 
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * https://www.github.com/JohnnyJayJay
@@ -18,9 +20,19 @@ public abstract class Tracker {
         this.okHttpClient = okHttpClient;
     }
 
-    protected void request(String url, Callback callback) {
-        Request request = new Request.Builder().header("TRN-Api-Key", apiKey).get().url(url).build();
+    protected void request(String endpoint, Callback callback) {
+        Request request = new Request.Builder().header("TRN-Api-Key", apiKey).get().url(endpoint).build();
         okHttpClient.newCall(request).enqueue(callback);
+    }
+
+    protected <T> T execute(String endpoint, Function<ResponseBody, T> function, Consumer<Exception> failure) {
+        Request request = new Request.Builder().header("TRN-Api-Key", apiKey).get().url(endpoint).build();
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            // TODO: 17.09.2018  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
